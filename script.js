6209eb5f68bcbf5b479305c06d8b9686eb6cdb33
Kendall Showalter <91501448+showaltk@users.noreply.github.com>
2021-11-30 08:24:45 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('.slide').slide({
+
+    // enable autoplay
+    start: false,
+
+    // loop through the images at a specified speed
+    speed: 3000,
+
+    // horizontal or fade
+    animate: "horizontal"
+  })
+;})
